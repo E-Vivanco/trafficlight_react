bd2fa348f09d76cba3038d317b0b5c95c10acd0c
E-Vivanco <vivanco.elsa@gmail.com>
2023-02-16 12:23:25 +0000

    ajustes 2 semaforo

--- src/App.js
+++ src/App.js
@@ -11,17 +11,17 @@ function App() {
       <div className='semaforo  mb-5 my-5'>
           <div className='row'>
                   <div className='col-12 col-md-12 col-sm-12 red_ligth'>
-                    <button className={'btn btn-outline-danger'+(color === 'red'? "active": "")} onClick={()=>setColor('red')}>                        
+                    <button className={'btn btn-outline-danger '+(color === 'red'? "active": " ")} onClick={()=>setColor('red')}>                        
                       <i className="fa-solid fa-circle mx-2" ></i></button>
                      
                   </div>
                    <div className='col-12 col-md-12 col-sm-12 amarilla_ligth'>
-                   <button className={'btn btn-outline-yellow '+(color === 'yellow'? "active": "")} onClick={()=>setColor('yellow')}>
+                   <button className={'btn btn-outline-yellow '+(color === 'yellow'? "active": " ")} onClick={()=>setColor('yellow')}>
                    <i className="fa-regular fa-circle mx-2"></i></button>
                       
                    </div>
                   <div className='col-12 col-md-12 col-sm-12 verde_ligth'>
-                  <button className={'btn btn-outline-success'+(color === 'green'? "active": "")} onClick={()=>setColor('green')}>
+                  <button className={'btn btn-outline-success '+(color === 'green'? "active": " ")} onClick={()=>setColor('green')}>
                   <i className="fa-regular fa-circle mx-2"></i></button>
                   </div>
           </div>
