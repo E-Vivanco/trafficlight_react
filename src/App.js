73332d3345e8b75010e3db91af913e3ee30f8e86
E-Vivanco <vivanco.elsa@gmail.com>
2023-02-16 04:31:19 +0000

    ajustes semaforo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import { useState } from 'react';
+import './index.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [color, setColor] =useState(" ")
+  return(
+  <>
+  
+  <div className='brazo_semaforo'>
+      <div className='semaforo  mb-5 my-5'>
+          <div className='row'>
+                  <div className='col-12 col-md-12 col-sm-12 red_ligth'>
+                    <button className={'btn btn-outline-danger'+(color === 'red'? "active": "")} onClick={()=>setColor('red')}>                        
+                      <i className="fa-solid fa-circle mx-2" ></i></button>
+                     
+                  </div>
+                   <div className='col-12 col-md-12 col-sm-12 amarilla_ligth'>
+                   <button className={'btn btn-outline-yellow '+(color === 'yellow'? "active": "")} onClick={()=>setColor('yellow')}>
+                   <i className="fa-regular fa-circle mx-2"></i></button>
+                      
+                   </div>
+                  <div className='col-12 col-md-12 col-sm-12 verde_ligth'>
+                  <button className={'btn btn-outline-success'+(color === 'green'? "active": "")} onClick={()=>setColor('green')}>
+                  <i className="fa-regular fa-circle mx-2"></i></button>
+                  </div>
+          </div>
+      </div>
+  </div>
+  </>
+ )
 }
 
 export default App;
